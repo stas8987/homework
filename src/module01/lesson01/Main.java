197f006f0ff0ac038477c6b76d37021645936b2b
stas8987 <1029384756-89@mail.ru>
2020-07-15 22:20:01 +0300

    Add new changes

---
+++ src/module01/lesson01/Main.java
@@ -0,0 +1,8 @@
+package module01.lesson01;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.print("Hello world \n");
+    }
+}
