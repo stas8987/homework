d755a9a2c0620cecb42550e27fe2bd72ef9283bb
stas8987 <1029384756-89@mail.ru>
2020-07-20 01:18:05 +0300

    Minor changes

--- src/module01/lesson01/Main.java
+++ src/module01/lesson01/Main.java
@@ -2,8 +2,23 @@ package module01.lesson01;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.print("Hello world \n");
+
+        for (int i = 2; i <= 100; i++) {
+            if (i==2||i==3){
+                System.out.print((i) + " ");
+            }
+            for (int b = 2; b < i - 1; b++) {
+                if ((i % b) != 0) {
+                    if (b==i-2)
+                        System.out.print((i) + " ");
+
+                }else {b=i-1;}
+            }
+           // System.out.println("Hello world");
+           // System.out.print("Hello world \n");
+            //поиск простых чисел 
+        }
     }
 }
+
 //First program
