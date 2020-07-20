dfd2e6562eab119a3604bbfe2dcc8125de366131
stas8987 <1029384756-89@mail.ru>
2020-07-20 20:20:55 +0300

    Minor changes

--- src/module01/lesson01/Main.java
+++ src/module01/lesson01/Main.java
@@ -1,5 +1,7 @@
 package module01.lesson01;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
          boolean a = false;
@@ -14,9 +16,23 @@ public class Main {
                 }
            // System.out.println("Hello world");
            // System.out.print("Hello world \n");
-            //поиск простых чисел 
+            //поиск простых чисел  перебором
         }
+        System.out.println("\n вторая версия");
+        int flag [] = new int[100+1];
+        for (int i = 2; i < flag.length; i++) {
+            if (flag[i]==0) {
+                System.out.print((i) + " ");
+                for (int h=i*i;h<flag.length;h+=i) {
+                    flag[h] = 1;
+                }
+            }
+        }
+
+
+
     }
+
 }
 
 //First program
